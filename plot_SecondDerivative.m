e775bf49951549bd449d63968ea8aec44368c71b
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2019-12-01 21:47:15 +0700

    Update plot_SecondDerivative.m

--- plot_SecondDerivative.m
+++ plot_SecondDerivative.m
@@ -6,27 +6,33 @@
 % Created by: Aulia Khalqillah,S.Si(2019)
 %
 % Load Data
-fwd = load('SecondForwardOut.txt');
-bwd = load('SecondBackwardOut.txt');
-cwd = load('SecondCenterOut.txt');
+fwd = load('SecondFdiffOut.txt');
+bwd = load('SecondBdiffOut.txt');
+cwd = load('SecondFdiffOut.txt');
 
 % Take element data of fwd
 f_iter = fwd(:,1);
 f_data = fwd(:,2);
 f_func = fwd(:,3);
 f_fwd = fwd(:,4);
+f_a = fwd(:,5);
+f_res = fwd(:,6);
 
 % Take element data of bwd
 b_iter = bwd(:,1);
 b_data = bwd(:,2);
 b_func = bwd(:,3);
 b_bwd = bwd(:,4);
+b_a = bwd(:,5);
+b_res = bwd(:,6);
 
 % Take element data of cwd
 c_iter = cwd(:,1);
 c_data = cwd(:,2);
 c_func = cwd(:,3);
 c_cwd = cwd(:,4);
+c_a = cwd(:,5);
+c_res = cwd(:,6);
 
 % Plot
 subplot(211)
