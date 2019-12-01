7c78b29cfc3197068c9920f4de02b27d4f757eb4
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2019-12-01 21:40:46 +0700

    Update SecondCenterDif.f95

--- SecondCenterDif.f95
+++ SecondCenterDif.f95
@@ -2,16 +2,16 @@
 ! gfortran -c SecondCenterDif.f95
 ! gfortran -o SecondCenterDif SecondCenterDif.o
 !
-! The program is open source and can use to numeric study purpose.
-! The program was build by Aulia Khalqillah,S.Si
+! The program is open source and can use be to numerical study purpose.
+! The program was written by Aulia Khalqillah,S.Si
 !
 ! email: auliakhalqillah.mail@gmail.com
 ! ==============================================================================
-PROGRAM SecondBackwardDifference
+PROGRAM CenterDifference
   IMPLICIT NONE
 
-  REAL :: A,B,H
-  REAL,DIMENSION(1000) :: F,FX,X
+  REAL :: A,B,H,X,RAWF,F
+  REAL,DIMENSION(1000) :: RAWFS,FA,FX,XS,RES
 
   INTEGER :: I,N
   CHARACTER(len=100) :: FMT
@@ -28,29 +28,31 @@ PROGRAM SecondBackwardDifference
   WRITE(*,"(a)",advance="no") "INSERT DATA LENGTH:"
   READ *, N
 
-
-  FMT = "(a12,a13,a15,a25)"
+  FMT = "(a12,a13,a20,a20,a15,a13)"
   WRITE(*,*) ""
-  WRITE(*,FMT)"ITER","Data(X)","F(X)","Second Center F(X)"
-  OPEN(10, FILE ='SecondCenterOut.txt', STATUS='replace')
-  ! Calculating residual between data point
+  WRITE(*,FMT)"ITER","Data(X)","Raw F(X)","Difference F(X)","Analytic F(X)","RESIDUAL"
+  OPEN(10, FILE ='SecondBdiffOut.txt', STATUS='replace')
+
+  ! Residual between two point is calculated
   H = (B-A)/N
-
-  ! Calculating Second Orde Backward Difference
-  X(1) = A
-  DO I = 1, N+2
-    F(I) = X(I)**3
-    X(I+1) = X(I) + H
+  X = A
+  I = 1
+  DO WHILE (I .le. N)
+    RAWF = F(X)
+    RAWFS(I) = RAWF ! Save the value
+    FX(I) = ((F(X+H)-(2*F(X))+F(X-H))/(H**2)) ! Second Center Difference is calculated
+    XS(I) = X ! Save the value
+    FA(I) = 6*XS(I)  ! Analytic Calculation of Second Difference
+    RES(I) = abs(real(FX(I))-real(FA(I))) ! Residual Calculation
+    WRITE(*,*) I,XS(I),RAWFS(I),FX(I),FA(I),RES(I)
+    WRITE(10,*) I,XS(I),RAWFS(I),FX(I),FA(I),RES(I)
+    X = X + H
+    I = I + 1
   END DO
-
-  DO I = 1,N
-    FX(I) = ((F(I+1) - (2*F(I)) + F(I-1))/(H**2))
-  END DO
-
-  DO I = 1,N
-    WRITE(*,*) I,X(I),F(I),FX(I)
-    WRITE(10,*) I,X(I),F(I),FX(I)
-  END DO
-  CLOSE(1)
-
 END PROGRAM
+
+REAL FUNCTION F(X)
+  IMPLICIT NONE
+  REAL :: X
+  F = X**3
+END FUNCTION
